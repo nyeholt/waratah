a5206b7ca5157502b27f35c4808991a43f4b6076
James Ellis (DPC) <james.ellis@dpc.nsw.gov.au>
2021-03-25 11:54:17 +1100

    Test for npm existence, variable-ise the various parameters such as gulp location, gulpfile location, add -q --silent to npm to reduce the bleating

--- build.sh
+++ build.sh
@@ -1,11 +1,28 @@
 #!/usr/bin/env bash
 NPM=`which npm`
-PREFIX=./themes/nswds/app/frontend
-echo "Building the NSW Design System via $NPM with target $PREFIX"
-echo "Installing"
-$NPM --prefix $PREFIX install
-echo "Rebuild node-sass"
-$NPM --prefix $PREFIX rebuild node-sass
-echo "gulp -f $PREFIX/gulpfile build"
-$PREFIX/node_modules/.bin/gulp -f $PREFIX/gulpfile.js build
+
+if ! [ -x "$(command -v $NPM)" ]; then
+  echo 'Error: npm is not installed on host' >&2
+  exit 1
+fi
+
+PWD=`pwd`
+PREFIX="${PWD}/themes/nswds/app/frontend/"
+GULP="${PREFIX}node_modules/.bin/gulp"
+GULPFILE="gulpfile.js"
+
+echo "Building the NSW Design System"
+echo "Binary: $NPM"
+echo "Prefix: $PREFIX"
+
+echo "Stage: installing"
+$NPM -q --silent --prefix $PREFIX install
+
+echo "Stage: rebuild node-sass"
+$NPM -q --silent --prefix $PREFIX rebuild node-sass
+
+echo "Stage: gulp build"
+echo "Binary: $GULP"
+echo "Gulpfile:${PREFIX}${GULPFILE}"
+$GULP -f ${PREFIX}${GULPFILE} build
 echo "Completed the NSW Design System build"
