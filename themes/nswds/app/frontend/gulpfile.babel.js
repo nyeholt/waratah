cbb33402852b5e55be37272f1267c98eec8575bf
James (DPC) <james.ellis@dpc.nsw.gov.au>
2021-07-16 12:59:30 +1000

    Fix errors in JS build, ensure concatenation of app.css, no need for merge stream, ensure dist directories are present in clean task

--- themes/nswds/app/frontend/gulpfile.babel.js
+++ themes/nswds/app/frontend/gulpfile.babel.js
@@ -5,7 +5,7 @@ import sourcemaps from 'gulp-sourcemaps';
 import rollup from 'gulp-better-rollup';
 import babel from 'rollup-plugin-babel';
 import cssnano from 'cssnano';
-import concatjs from 'gulp-concat';
+import concat from 'gulp-concat';
 import svgSprite from 'gulp-svg-sprite';
 import uglifyjs from 'gulp-uglify-es';
 import resolve from 'rollup-plugin-node-resolve'
@@ -13,18 +13,25 @@ import commonjs from 'rollup-plugin-commonjs'
 import del from 'del';
 import rename from 'gulp-rename';
 import filter from 'gulp-filter';
-import mergeStream from 'merge-stream';
+import gulpDebug from 'gulp-debug';
+import mkdirp from 'mkdirp';
+
+const designSystem = {
+  'src': {
+    'js': './node_modules/nsw-design-system/dist/js/main.js',
+    'css': ''
+  }
+};
 
 const config = {
     'src': {
         'css': [
-          './node_modules/nsw-design-system/dist/css/main.css',
           './src/scss/app.scss',
           // the relative path to the Silverstripe app folder
           '../../../../../../../mysite/frontend/src/scss/app.scss'
         ],
         'js': [
-          './node_modules/nsw-design-system/dist/js/main.js',
+          designSystem.src.js,
           './src/js/app.js',
           // the relative path to the Silverstripe app folder
           '../../../../../../../mysite/frontend/src/js/app.js'
@@ -50,49 +57,57 @@ const config = {
 }
 
 gulp.task('clean', function () {
-    return del(['dist/*'], { dot: true })
+    gulpDebug( {'title': 'CLEAN' } );
+    let delDist = del([ 'dist/*' ], { dot: true } );
+    let mkdirCSS = mkdirp(config.dist.css);
+    gulpDebug( {'title': 'mkdirp:' + config.dist.css } );
+    let mkdirJS = mkdirp(config.dist.js);
+    gulpDebug( {'title': 'mkdirp:' + config.dist.js } );
+    let mkdirSVG = mkdirp(config.dist.svg);
+    gulpDebug( {'title': 'mkdirp:' + config.dist.svg } );
+    return delDist && mkdirCSS && mkdirJS && mkdirSVG;
 });
 
-
 gulp.task('scss', function () {
-    let stream = mergeStream();
 
-    config.src.css.forEach((item, i) => {
-
-      stream.add( gulp.src(item)
-          .pipe(sass().on('error', sass.logError))
-          .pipe(sourcemaps.init())
-          .pipe(sourcemaps.write('.'))
-          // output non-minified
-          .pipe(gulp.dest(config.dist.css))
-          .pipe(filter('**/*.css'))
-          // minfify
-          .pipe(postcss([
-              cssnano()
-          ]))
-          .pipe(rename({ suffix: '.min' }))
-          .pipe(gulp.dest(config.dist.css))
-      );
-    });
-
-    return stream;
+    return gulp.src(config.src.css)
+      .pipe( gulpDebug( {'title': 'SCSS process:' + config.src.css }))
+      .pipe(sass().on('error', sass.logError))
+      .pipe(sourcemaps.init())
+      .pipe( gulpDebug( {'title': 'concat to app.css' }))
+      .pipe(concat('app.css'))
+      .pipe(sourcemaps.write('.'))
+      // output non-minified
+      .pipe(gulp.dest(config.dist.css))
+      .pipe(filter('**/*.css'))
+      // minify
+      .pipe( gulpDebug( {'title': 'minify app.css' }))
+      .pipe(postcss([
+          cssnano()
+      ]))
+      .pipe(rename({ suffix: '.min' }))
+      .pipe( gulpDebug( {'title': 'write minified app.css' }))
+      .pipe(gulp.dest(config.dist.css));
 
 });
 
 gulp.task('svg', function () {
+
     return gulp.src(config.src.svg)
+        .pipe( gulpDebug( {'title': 'SVG process:' + config.src.svg }))
         .pipe(svgSprite(config.src.svgSprite))
         .on('error', (error) => {
             console.log(error)
         })
-        .pipe(gulp.dest(config.dist.svg))
+        .pipe( gulpDebug( {'title': 'write SVG dist' }))
+        .pipe(gulp.dest(config.dist.svg));
+
 });
 
 gulp.task('js', function () {
-  let stream = mergeStream();
 
-  config.src.css.forEach((item, i) => {
-    stream.add( gulp.src(config.src.js)
+    return gulp.src(config.src.js)
+        .pipe( gulpDebug( {'title': 'JS process' + config.src.js }))
         .pipe(
             rollup(
                 {
@@ -104,22 +119,20 @@ gulp.task('js', function () {
             )
         )
         .pipe(sourcemaps.init())
-        .pipe(concatjs('app.js'))
+        .pipe( gulpDebug( {'title': 'concat app.js' }))
+        .pipe(concat('app.js'))
         // output non-uglified
         .pipe(sourcemaps.write('.'))
         .pipe(gulp.dest(config.dist.js))
         // filter only JS files
         .pipe(filter('**/*.js'))
         // uglify
+        .pipe( gulpDebug( {'title': 'uglify app.js' }))
         .pipe(uglifyjs())
         .pipe(rename({ suffix: '.min' }))
-        .pipe(gulp.dest(config.dist.js))
+        .pipe( gulpDebug( {'title': 'write minified app.js' }))
+        .pipe(gulp.dest(config.dist.js));
 
-      );
-
-    });
-
-    return stream;
 })
 
 gulp.task('watch', function () {
--- themes/nswds/app/frontend/src/js/app.js
+++ themes/nswds/app/frontend/src/js/app.js
@@ -6,19 +6,15 @@ import initStickyJS from './components/stickyjs';
 
 function initAppMain() {
     try {
-
         // remove no-js
         (function(e){e.className=e.className.replace(/\bno-js\b/,'js')})(document.documentElement);
-
         // init the site
         window.NSW.initSite();
-
         // init components
         initMicroModal();
         initSlimSelect();
         initFlatPickr();
         initStickyJS();
-
     } catch (e) {
         console.warn(e);
     }
