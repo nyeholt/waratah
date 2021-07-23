6030d301a28aff23fe78dc1af3ed744bb08b3c38
James (DPC) <james.ellis@dpc.nsw.gov.au>
2021-07-23 16:06:37 +1000

    Move generic tinymce config to the content boilerplate module

--- _config.php
+++ _config.php
@@ -1,36 +1,5 @@
 <?php
 use SilverStripe\Forms\HTMLEditor\HtmlEditorConfig;
-use SilverStripe\Forms\HTMLEditor\TinyMCEConfig;
-
-TinyMCEConfig::get('cms')->removeButtons('underline', 'alignjustify');
-
-TinyMCEConfig::get('cms')->enablePlugins(
-    'visualblocks',
-    'hr',
-    'anchor',
-    'wordcount',
-    'lists',
-    'advlist'
-);
-
-TinyMCEConfig::get('cms')->insertButtonsAfter(
-    'removeformat',
-    '|',
-    'superscript',
-    'subscript',
-    'blockquote',
-    'hr'
-);
-TinyMCEConfig::get('cms')->addButtonsToLine(1, '|', 'visualblocks');
-
-TinyMCEConfig::get('cms')->insertButtonsAfter('formatselect', 'styleselect');
-TinyMCEConfig::get('cms')->insertButtonsAfter('unlink', 'anchor');
-
-TinyMCEConfig::get('cms')->setOptions([
-    'extended_valid_elements' => 'ol[start]',
-]);
-
-HtmlEditorConfig::get('cms')->setOption('theme_advanced_styles', 'highlight=highlight;no-border=no-border,break=break');
 
 HtmlEditorConfig::get('cms')->setOption('table_class_list', [
     [
