2675c92ad7d3b636e8140935f9b678906410d7c4
James (DPC) <james.ellis@dpc.nsw.gov.au>
2021-10-18 18:28:24 +1100

    Add a gallery frontend loader for nswds/Media support

---
+++ src/Services/GalleryFrontend.php
@@ -0,0 +1,42 @@
+<?php
+
+namespace NSWDPC\Waratah\Services;
+
+use NSWDPC\Elemental\Services\QuickGallery\Frontend;
+
+use SilverStripe\View\Requirements;
+
+/**
+ * Provides a basic frontend using Slick
+ * Apply your own frontend and/or frontend loader using Injector
+ * @author James
+ */
+class GalleryFrontend extends Frontend {
+
+    /**
+     * Apply loader to the relevant slideshow classes
+     * In the context of nswds, the gallery frontend is applied to any
+     * element with a data-gallery attribute, it must contain figure elements
+     * from nswds/Media
+     */
+    public function addLoader() {
+
+        $anchor = $this->element->getAnchor();
+
+        $script = <<<JS
+$(document).ready(function(){
+    $('#{$anchor} [data-gallery]').slickLightbox({
+        itemSelector: 'figure > a:first-child',
+        caption: function(element, info) {
+            console.log(element);
+            return $(element).next('figcaption').text()
+        },
+        captionPosition: 'dynamic',
+        lazy: true
+    });
+});
+JS;
+        Requirements::customScript($script, "gallery-{$anchor}");
+
+    }
+}
