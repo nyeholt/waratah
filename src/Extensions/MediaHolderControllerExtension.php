417d170f7fd5e98a2bba70d25658f62ad4964ac8
Mark Taylor <mark.taylor@dpc.nsw.gov.au>
2021-10-21 12:42:24 +1100

    Add extension (that doesn't work)

--- src/Extensions/MediaHolderControllerExtension.php
+++ src/Extensions/MediaHolderControllerExtension.php
@@ -3,21 +3,20 @@
 namespace NSWDPC\Waratah\Extensions;
 
 use SilverStripe\ORM\DataExtension;
-use nglasl\mediawesome\MediaPage;
-use SilverStripe\ORM\ArrayList;
 
 class MediaHolderControllerExtension extends DataExtension {
 
 
-    public function PaginatedChildrenWithoutRelated($limit = 12, $sort = 'Date', $order = 'DESC')
-    {
-        return $this->owner->getPaginatedChildren($limit, $sort, $order)->exclude('ParentMediaPageID:not', 0);
-
-    }
+//     public function PaginatedChildrenWithoutRelated($limit = 12, $sort = 'Date', $order = 'DESC')
+//     {
+//         return $this->owner->getPaginatedChildren($limit, $sort, $order)->exclude('ParentMediaPageID:not', 0);
+//
+//     }
 
     public function updatePaginatedChildren($children)
     {
         $children = $children->exclude('ParentMediaPageID:not', 0);
+
     }
 
 
