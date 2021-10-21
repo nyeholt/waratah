4b5e697912b675e83978f11a0dfb8313c8da2c77
Mark Taylor <mark.taylor@dpc.nsw.gov.au>
2021-10-21 14:12:26 +1100

    Remove my stupid code

--- src/Extensions/MediaHolderControllerExtension.php
+++ src/Extensions/MediaHolderControllerExtension.php
@@ -6,18 +6,4 @@ use SilverStripe\ORM\DataExtension;
 
 class MediaHolderControllerExtension extends DataExtension {
 
-
-//     public function PaginatedChildrenWithoutRelated($limit = 12, $sort = 'Date', $order = 'DESC')
-//     {
-//         return $this->owner->getPaginatedChildren($limit, $sort, $order)->exclude('ParentMediaPageID:not', 0);
-//
-//     }
-
-    public function updatePaginatedChildren($children)
-    {
-        $children = $children->exclude('ParentMediaPageID:not', 0);
-
-    }
-
-
 }
