03273f37d11a1baf005b9fad7dfc95efbcb9233d
James (DPC) <james.ellis@dpc.nsw.gov.au>
2021-11-25 14:31:25 +1100

    ENH: provide some singular and plural names for the page add tool

--- src/Pages/HomePage.php
+++ src/Pages/HomePage.php
@@ -4,5 +4,16 @@ namespace NSWDPC\Waratah\Pages;
 
 class HomePage extends \Page
 {
+    /**
+     * @var string
+     */
+    private static $singular_name= "Home page";
+
+    /**
+     * @var string
+     */
+    private static $plural_name = "Home pages";
+
     private static $table_name = 'HomePage';
+
 }
