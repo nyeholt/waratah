3d54a02b5a2456fe621cf13333777c20d08c0329
James (DPC) <james.ellis@dpc.nsw.gov.au>
2021-12-02 12:56:44 +1100

    NEW: HeroBannerBrandSelectionField

---
+++ src/Forms/HeroBannerBrandSelectionField.php
@@ -0,0 +1,14 @@
+<?php
+
+namespace NSWDPC\Waratah\Forms;
+
+use NSWDPC\Waratah\Traits\DesignSystemSelections;
+use SilverStripe\Forms\DropdownField;
+
+/**
+ * Extend Card selection for brand to HeroBanner brand selection
+ * @author James
+ */
+class HeroBannerBrandSelectionField extends CardBrandSelectionField
+{
+}
